e6040cb599d7080c6edfc87c96bfdbf5269e708a
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-05-10 16:45:54 -0600

    add ProductDetailsComponent and initial routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 
@@ -12,7 +12,10 @@ import { TopBarComponent } from './top-bar/top-bar.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+    ]),
     ProductAlertsComponent,
   ],
   declarations: [AppComponent, TopBarComponent, ProductListComponent],
