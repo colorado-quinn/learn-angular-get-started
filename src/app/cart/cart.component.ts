afaab71857747b9b74930cde75eab783bdb0028e
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-05-13 11:25:45 -0600

    add cart component and route

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,16 @@
+import { CurrencyPipe, NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-cart',
+  standalone: true,
+  imports: [NgFor, CurrencyPipe],
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.css',
+})
+export class CartComponent {
+  items = this.cartService.getItems();
+
+  constructor(private cartService: CartService) {}
+}
