76c4c0c1e1e1d10d787eb8a83b577a0722fad4e8
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-05-13 22:57:36 -0600

    add checkout

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,17 +1,33 @@
 import { CurrencyPipe, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [NgFor, CurrencyPipe, RouterLink],
+  imports: [NgFor, CurrencyPipe, RouterLink, ReactiveFormsModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
 export class CartComponent {
   items = this.cartService.getItems();
 
-  constructor(private cartService: CartService) {}
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
