aa1802a92c10f0c48db600d0875b2b3fa4364423
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-05-16 11:50:05 -0600

    add initial CartService test

--- src/app/cart.service.spec.ts
+++ src/app/cart.service.spec.ts
@@ -6,13 +6,24 @@ describe('CartService', () => {
   let service: CartService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [provideHttpClient()],
-    });
+    TestBed.configureTestingModule({ providers: [provideHttpClient()] });
     service = TestBed.inject(CartService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add a product to the cart', () => {
+    expect(service.items).toHaveSize(0);
+
+    service.addToCart({
+      id: 1,
+      name: 'test',
+      price: 5,
+      description: 'this is a test product',
+    });
+
+    expect(service.items).toHaveSize(1);
   });
 });
