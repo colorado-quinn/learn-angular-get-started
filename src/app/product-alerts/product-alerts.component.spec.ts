0c283f4da528753b5ffaf5293b000b7829ad7c3a
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-05-17 19:24:44 -0600

    add component dom  tests

--- src/app/product-alerts/product-alerts.component.spec.ts
+++ src/app/product-alerts/product-alerts.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductAlertsComponent } from './product-alerts.component';
 
 describe('ProductAlertsComponent', () => {
-  let component: ProductAlertsComponent;
+  let sut: ProductAlertsComponent;
   let fixture: ComponentFixture<ProductAlertsComponent>;
 
   beforeEach(async () => {
@@ -12,11 +12,44 @@ describe('ProductAlertsComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductAlertsComponent);
-    component = fixture.componentInstance;
+    sut = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(sut).toBeTruthy();
+  });
+
+  it('should show button for product costing 701', () => {
+    sut.product = {
+      id: 1,
+      description: 'test',
+      name: 'Name1',
+      price: 701,
+    };
+    fixture.detectChanges();
+
+    const productAlertsElement: HTMLElement = fixture.nativeElement;
+    const button = productAlertsElement.querySelector('button');
+    expect(button)
+      .withContext('"Notify me" button should be shown')
+      .toBeTruthy();
+    expect(button?.textContent).toEqual('Notify Me');
+  });
+
+  it('should not show button for product costing 700', () => {
+    sut.product = {
+      id: 1,
+      description: 'test',
+      name: 'Name1',
+      price: 700,
+    };
+    fixture.detectChanges();
+
+    const productAlertsElement: HTMLElement = fixture.nativeElement;
+    const button = productAlertsElement.querySelector('button');
+    expect(button)
+      .withContext('"Notify me" button should NOT be shown')
+      .toBeNull();
   });
 });
